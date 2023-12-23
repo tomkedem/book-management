6e32107739eecfaf654647182d1c261f0dde651a
Tomer Kedem <tompha1@hotmail.com>
2023-12-23 15:04:26 +0200

    Creating success and failure actions

--- src/app/books/book.actions.ts
+++ src/app/books/book.actions.ts
@@ -2,4 +2,9 @@ import { createAction, props } from "@ngrx/store";
 import { Book } from "../model/book";
 
 export const AddBook = createAction('[Book] Add Book', props<Book>());
+export const AddBookSuccess = createAction('[Book] Added Book Successfuly', props<Book>());
+export const AddBookFailure = createAction('[Book] Added Book Failure', props<{error: any}>());
+
+
+
 export const RemoveBook = createAction('[Book] Remove Book', props<{bookId: string}>());
