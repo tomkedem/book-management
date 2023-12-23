64d2f76bcb2ab0cec57ca7d574fafdc5d271ea5f
Tomer Kedem <tompha1@hotmail.com>
2023-12-23 09:41:19 +0200

    Creating the store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { BookReducer } from './books/book.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({book: BookReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
