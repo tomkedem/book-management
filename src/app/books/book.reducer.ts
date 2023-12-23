5159ce032c0eff44552a2b76e82ef7a082e7faef
Tomer Kedem <tompha1@hotmail.com>
2023-12-23 15:10:26 +0200

    Adjusting the reducer for effects

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,12 +1,19 @@
 import { State, createReducer, on } from "@ngrx/store";
-import { AddBook, RemoveBook } from "./book.actions";
+import { AddBook, RemoveBook, AddBookSuccess, AddBookFailure } from "./book.actions";
 import { Book } from "../model/book";
+import { state } from "@angular/animations";
 
 export const initialState: Book[] = [];
 
 export const BookReducer = createReducer (
     initialState, 
-    on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
+    on(AddBook, (state) => {return state}),
+    on(AddBookSuccess, (state, {id, title, author}) => [...state, {id, title, author}]),
+    on(AddBookFailure,(state, {error}) => {
+        console.error(error);
+        return state;
+    }),
+
     on(RemoveBook, (state, {bookId}) => state.filter(book => book.id !==bookId))
 );
     
