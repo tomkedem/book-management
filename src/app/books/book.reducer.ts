10a2fc6d08f2ff4d3ecca611b7337a204bcf4b37
Tomer Kedem <tompha1@hotmail.com>
2023-12-23 09:29:31 +0200

    Creating the book reducer part 1: Creating a reducer

---
+++ src/app/books/book.reducer.ts
@@ -0,0 +1,11 @@
+import { createReducer, on } from "@ngrx/store";
+import { AddBook, RemoveBook } from "./book.actions";
+import { Book } from "../model/book";
+
+export const initialState: ReadonlyArray<Book> = [];
+
+export const BookReduser = createReducer (
+    initialState
+)
+    
+
